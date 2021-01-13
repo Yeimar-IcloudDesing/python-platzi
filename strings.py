d21d97bbb6c2c8c12dddb92259e2545a4113b1b9
yeimar <yeimar112003@outlook.com>
2021-01-12 22:38:43 -0500

    Se agregaron los archivos strings.py y variables.py

---
+++ strings.py
@@ -0,0 +1,16 @@
+mystr = "Hello World"
+
+# #print(dir(mystr))
+# print(mystr.lower())
+# print(mystr.upper())
+# print(mystr.swapcase())
+# print(mystr.capitalize())
+# print(mystr.startswith('Hello'))
+# print(mystr.endswith('ld'))
+# print(mystr.split)
+
+name = "Yeimar"
+
+print("My name is" + name) # Con el más concatenamos los strings
+print(f"My name is {name}")  # Con el f le decimos que dentro de los strings hay una variable la cual depositamos dentro de llaves.
+print("My name is {0}" .format(name)) # con el .format le decimos que reconozca la pocision 0 de la tupla-
---
+++ variables.py
@@ -0,0 +1,20 @@
+name = "Yeimar"
+print(name) # imprime el valor de la variable 
+
+#Case sensitive
+book = "El lobo"
+Book = " 100% Max "
+print(book)
+print(Book)
+# en python la variable book y Book, son totalmente diferentes ya que aplica case sensitive...
+
+x,name = 100, "Yeimar" # Definimos en una sola linea
+
+print(x,name); # Imprimimos en una sola linea
+
+
+# Conventions
+
+book_name = "I robot" #snake Case
+bookName = "El robo del siglo " #Camel case
+BookName = "ra" #pascal case
